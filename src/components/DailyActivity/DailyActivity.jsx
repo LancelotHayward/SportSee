73d96aab5d8c033eac13c8b2b935615e16e4934b
LancelotHayward <hayward.lance@gmail.com>
2024-05-27 22:22:33 +0200

    worked on recharts

--- src/components/DailyActivity/DailyActivity.jsx
+++ src/components/DailyActivity/DailyActivity.jsx
@@ -1,17 +1,18 @@
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts'
+import { BarChart, CartesianGrid, Tooltip, Legend, Bar, ResponsiveContainer, XAxis, YAxis } from 'recharts'
 import "./DailyActivity.scss"
 
 function DailyActivity() {
     return (
-        <BarChart width={730} height={250} data={{ name: 'a', value: [5, 12] }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="Poids" fill="#8884d8" />
-            <Bar dataKey="Calories" fill="#82ca9d" />
-        </BarChart>
+        // <ResponsiveContainer width={"100%"} height={"100%"}>
+            <BarChart width={730} height={250} data={[{day: "1", poids:"68", calories:"356"},{day: "2", poids:"65", calories:"400"}]}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip/>
+                <Legend />
+                <XAxis dataKey="day"/>
+                <Bar dataKey="poids" fill="#8884d8" />
+                <Bar dataKey="calories" fill="#82ca9d" />
+            </BarChart>
+        // </ResponsiveContainer>
     )
 }
 
