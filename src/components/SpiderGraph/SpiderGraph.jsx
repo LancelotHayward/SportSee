acba42a1c6239c509bd2c6b7aeca76d01eceb5b7
LancelotHayward <hayward.lance@gmail.com>
2024-06-07 11:12:47 +0200

    Overall CSS Pass

--- src/components/SpiderGraph/SpiderGraph.jsx
+++ src/components/SpiderGraph/SpiderGraph.jsx
@@ -5,12 +5,14 @@ import "./SpiderGraph.scss"
 
 function SpiderGraph({data}) {
   return (
-    <RadarChart outerRadius={90} width={260} height={260} data={data}>
+    <div id="spidergraph">
+      <RadarChart outerRadius={90} width={260} height={260} data={data}>
       <PolarGrid />
       <PolarAngleAxis dataKey="stat" />
       <PolarRadiusAxis angle={30} domain={[0, 7]} />
       <Radar dataKey="level" stroke="#FF0101" fill="#FF0101" fillOpacity={0.6} />
-    </RadarChart>
+      </RadarChart>
+    </div>
   )
 }
 
